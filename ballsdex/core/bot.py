464cf057e9432a15e2a6cf39a324530cc922aaf1
xAshleyy <74438582+xAshleyy@users.noreply.github.com>
2026-06-18 18:48:40 -0700

    Update bot.py

--- ballsdex/core/bot.py
+++ ballsdex/core/bot.py
@@ -60,6 +60,7 @@ DEFAULT_PACKAGES = (
     ("money", "ballsdex.packages.money"),
     ("players", "ballsdex.packages.players"),
     ("trade", "ballsdex.packages.trade"),
+    ("crafting", "ballsdex.packages.crafting"),
 )
 
 
